035ff9559b65f38bf20a900dc8ac4c6af42807d9
Dina-Ursula <dinatyan26@gmail.com>
2022-12-12 22:57:31 +0200

    completed-5

---
+++ js/task-05.js
@@ -0,0 +1,19 @@
+
+const inputName = document.querySelector('#name-input');
+const spanOutput = document.querySelector('#name-output');
+
+inputName.addEventListener('input', inputValue);
+function inputValue(evt) {
+    spanOutput.textContent = evt.currentTarget.value;
+}
+
+inputName.addEventListener('focus', onFocus);
+inputName.addEventListener('blur', onBlur);
+
+function onFocus() {
+    console.log('інпут в фокусі');
+};
+function onBlur() {
+        console.log('інпут в блюрі');
+
+};
