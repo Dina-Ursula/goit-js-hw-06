d9c021e771cdf311ad0b99dc882891c143be0c02
Dina-Ursula <dinatyan26@gmail.com>
2023-01-12 12:35:21 +0200

    edits-5

--- js/task-05.js
+++ js/task-05.js
@@ -1,17 +1,23 @@
 const inputName = document.querySelector("#name-input");
 const spanOutput = document.querySelector("#name-output");
 
+const defaultText = spanOutput.textContent;
 inputName.addEventListener("input", inputValue);
 function inputValue(evt) {
-  spanOutput.textContent = evt.currentTarget.value;
+  //   if (evt.currentTarget.value !== "") {
+  //     return (spanOutput.textContent = evt.currentTarget.value);
+  //   }
+  //   return (spanOutput.textContent = defaultText);
+  // }
+  spanOutput.textContent =
+    evt.currentTarget.value !== "" ? evt.currentTarget.value : defaultText;
 }
 
-inputName.addEventListener("focus", onFocus);
-inputName.addEventListener("blur", onBlur);
+// inputName.addEventListener("focus", onFocus);
+// inputName.addEventListener("blur", onBlur);
 
-function onFocus() {
-  console.log("інпут в фокусі");
-}
-function onBlur() {
-  console.log("інпут в блюрі");
-}
+// function onFocus() {
+//   console.log("інпут в фокусі");
+// }
+// function onBlur() {
+//   console.log("інпут в блюрі");
